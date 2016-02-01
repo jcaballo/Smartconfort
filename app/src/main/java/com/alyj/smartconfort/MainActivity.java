47391be1d9a012b44e7a58b6cea3cb7558fd553e
caballoj <julien.caballol@hotmail.fr>
2016-02-01 17:26:19 +0100

    Ajout de sphynx

--- app/src/main/java/com/alyj/smartconfort/MainActivity.java
+++ app/src/main/java/com/alyj/smartconfort/MainActivity.java
@@ -1,13 +1,31 @@
 package com.alyj.smartconfort;
 
-import android.support.v7.app.ActionBarActivity;
+import android.speech.SpeechRecognizer;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity
+         {
+
+    private TextView returnedText;
+    private ToggleButton toggleButton;
+    private ProgressBar progressBar;
+    private SpeechRecognizer speech = null;
+    private Intent recognizerIntent;
+    private String LOG_TAG = "VoiceRecognitionActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
 }
